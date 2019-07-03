d3ff605981b2b9227568ba597a2791647bbb61fc
Administrator <admin@example.com>
2019-07-03 17:24:06 +0800

    创建登陆页面

--- src/main/webapp/app/modules/register/register-modal.tsx
+++ src/main/webapp/app/modules/register/register-modal.tsx
@@ -11,7 +11,7 @@ export interface ILoginModalProps {
   handleClose: Function;
 }
 
-class LoginModal extends React.Component<ILoginModalProps> {
+class RegisterModal extends React.Component<ILoginModalProps> {
   handleSubmit = (event, errors, { username, password, rememberMe }) => {
     const { handleLogin } = this.props;
     handleLogin(username, password, rememberMe);
@@ -24,48 +24,73 @@ class LoginModal extends React.Component<ILoginModalProps> {
       <Modal isOpen={this.props.showModal} toggle={handleClose} backdrop="static" id="login-page" autoFocus={false}>
         <AvForm onSubmit={this.handleSubmit}>
           <ModalHeader id="login-title" toggle={handleClose}>
-            <Translate contentKey="login.title">Sign in</Translate>
+            <span>注册</span>
           </ModalHeader>
           <ModalBody>
             <Row>
               <Col md="12">
                 {loginError ? (
                   <Alert color="danger">
-                    <Translate contentKey="login.messages.error.authentication">
-                      <strong>Failed to sign in!</strong> Please check your credentials and try again.
-                    </Translate>
+                    <span>操作失败，请正确输入注册信息.</span>
                   </Alert>
                 ) : null}
               </Col>
               <Col md="12">
+                <button
+                  style={{
+                    float: 'right',
+                    backgroundColor: '#fe4365',
+                    color: '#fffde5',
+                    borderRadius: '0.25rem',
+                    border: '1px solid #fe4365',
+                    width: '40%',
+                    marginTop: '30px',
+                    height: '40px'
+                  }}
+                >
+                  发送验证码
+                </button>
                 <AvField
                   name="username"
-                  label={translate('global.form.username.label')}
-                  placeholder={translate('global.form.username.placeholder')}
+                  label={'手机号'}
+                  placeholder={'请输入手机号'}
                   required
                   errorMessage="用户名不能为空!"
                   autoFocus
+                  style={{
+                    width: '55%'
+                  }}
+                />
+                <AvField
+                  name="code"
+                  label={<span style={{ float: 'left', marginTop: '7px' }}>验证码：</span>}
+                  placeholder={'请输入验证码'}
+                  required
+                  errorMessage="验证码不能为空!"
+                  style={{ width: '70%', float: 'right' }}
                 />
                 <AvField
                   name="password"
                   type="password"
-                  label={translate('login.form.password')}
-                  placeholder={translate('login.form.password.placeholder')}
+                  label={<span style={{ float: 'left', marginTop: '7px' }}>密码：</span>}
+                  placeholder={'请输入密码'}
                   required
                   errorMessage="密码不能为空!"
+                  style={{ width: '70%', float: 'right' }}
+                />
+                <AvField
+                  name="repassword"
+                  type="password"
+                  label={<span style={{ float: 'left', marginTop: '7px' }}>重复密码：</span>}
+                  placeholder={'请重复输入密码'}
+                  required
+                  errorMessage="重复密码不能为空!"
+                  style={{ width: '70%', float: 'right' }}
                 />
                 <AvGroup check inline>
                   <Label className="form-check-label">
-                    <AvInput type="checkbox" name="rememberMe" /> <Translate contentKey="login.form.rememberme">Remember me</Translate>
-                  </Label>
-                </AvGroup>
-                <AvGroup check inline style={{ float: 'right' }}>
-                  <Label className="form-check-label">
-                    <span>
-                      <Link to={'/register'} className="alert-link" style={{ color: '#fe4365', textDecoration: 'underline' }}>
-                        注册账户
-                      </Link>
-                    </span>
+                    <AvInput type="checkbox" name="rememberMe" />
+                    我已阅读并同意《用户协议》
                   </Label>
                 </AvGroup>
               </Col>
@@ -89,7 +114,7 @@ class LoginModal extends React.Component<ILoginModalProps> {
               }}
               type="submit"
             >
-              <Translate contentKey="login.form.button">Sign in</Translate>
+              注册
             </Button>
           </ModalFooter>
         </AvForm>
@@ -98,4 +123,4 @@ class LoginModal extends React.Component<ILoginModalProps> {
   }
 }
 
-export default LoginModal;
+export default RegisterModal;
