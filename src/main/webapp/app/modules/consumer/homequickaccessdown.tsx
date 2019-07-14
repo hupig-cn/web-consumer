92aec525f3d3e9fd327c4b9e7fc584f075bd61ff
Daniey <lixin520gj@163.com>
2019-07-15 07:13:28 +0800

    增加商家二维码保存

--- src/main/webapp/app/modules/consumer/homequickaccessdown.tsx
+++ src/main/webapp/app/modules/consumer/homequickaccessdown.tsx
@@ -6,6 +6,7 @@ import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 // tslint:disable-next-line: no-submodule-imports
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 import { toast } from 'react-toastify';
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -66,7 +67,9 @@ export default function LongMenu() {
   return (
     <div style={{ paddingTop: '12px', backgroundColor: 'white' }}>
       <BottomNavigation showLabels className={classes.root} value={value} onChange={handleChange}>
+        <Link to="/exhibitionpage">
         <BottomNavigationAction label="积分精选" value="key1" icon={<img src="./content/images/icon1.png" />} />
+        </Link>
         <BottomNavigationAction label="热门促销" value="key2" icon={<img src="./content/images/icon2.png" />} />
         <BottomNavigationAction label="团购" value="key3" icon={<img src="./content/images/icon3.png" />} />
         <BottomNavigationAction label="出行" value="key4" icon={<img src="./content/images/icon4.png" />} />
--- src/main/webapp/app/modules/consumer/incomepage/exhibitionpage.tsx
+++ src/main/webapp/app/modules/consumer/incomepage/exhibitionpage.tsx
@@ -1,9 +1,55 @@
 import React from 'react';
 import QRCode from 'qrcode.react';
 import Title from './title';
+import { Button } from '@material-ui/core';
+import { IRootState } from 'app/shared/reducers';
+import { getSession } from 'app/shared/reducers/authentication';
+import { getMyEntityMerchant, resetMerchant } from 'app/requests/merchant/merchant.reducer';
+import { connect } from 'react-redux';
 
-export class Exhibitionpage extends React.Component {
+export interface IExhibitionpageProp extends StateProps, DispatchProps {
+}
+
+export class Exhibitionpage extends React.Component<IExhibitionpageProp> {
+  componentDidMount() {
+    this.props.getSession();
+    this.props.resetMerchant();
+    this.props.getMyEntityMerchant(this.props.account.id);
+  }
+
   render() {
+    const { account, merchantEntity } = this.props;
+
+    function loadbgimg(name) {
+      var qrcode = document.getElementById('qrcode-canvas-key') as HTMLCanvasElement;// 二维码
+      var bgImg = document.getElementById('qrcode-image') as HTMLImageElement;
+      var qrcodeImg = new Image();
+      if (qrcode === null) return null;
+      qrcodeImg.src = qrcode.toDataURL("image/png");
+      qrcodeImg.onload = function () {
+        var canvas = document.getElementById("myCanvas") as HTMLCanvasElement;// 空画板
+        var ctx = canvas.getContext("2d");
+        var patBg = ctx.createPattern(bgImg, "repeat");
+        ctx.rect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = patBg;
+        ctx.fill();
+        ctx.drawImage(qrcodeImg, 90, 264, 233, 233);
+        ctx.font = "24px bold 黑体";
+        ctx.fillStyle = "#fe4365";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillText(name, 200, 530);
+        var overImg = new Image();
+        overImg.src = canvas.toDataURL("image/png");
+        overImg.onload = function () {
+          var saveImage = document.getElementById('saveImage') as HTMLCanvasElement;// 显示位置
+          saveImage.innerHTML = "";
+          saveImage.append(overImg);
+        };
+      };
+      return null;
+    }
+
     return (
       <div
         style={{
@@ -15,7 +61,7 @@ export class Exhibitionpage extends React.Component {
           paddingTop: '35px'
         }}
       >
-        <Title />
+        <Title/>
         <div
           style={{
             width: '100%',
@@ -23,22 +69,37 @@ export class Exhibitionpage extends React.Component {
             position: 'relative'
           }}
         >
-          <img style={{}} src="./content/images/income.png" />
-          <div
-            style={{
-              float: 'left',
-              position: 'absolute',
-              top: '264px',
-              left: '91px',
-              textAlign: 'center'
-            }}
-          >
-            <QRCode value={'http://app.yuanscore.com:8080/#id=1'} size={232} fgColor="#000000" bgColor="#ffffff" />
+          <div style={{ display: "none" }}>
+            <
+              // @ts-ignore
+              QRCode id="qrcode-canvas-key" value={'http://app.yuanscore.com:8080/#id=' + merchantEntity.id}
+                     renderAs='canvas'
+                     size={233} fgColor="#000000" bgColor="#ffffff"/>
+            <canvas id="myCanvas" width="414" height="621"></canvas>
+            <img id="qrcode-image" src="./content/images/income.png"/>
           </div>
+          <div id="saveImage"></div>
+          <Button variant="contained" color={"secondary"}
+                  style={{ position: "fixed", top: '90%', left: '20%', width: "60%" }}
+                  onClick={loadbgimg(merchantEntity.name)}>预览(长按图像，保存到本地)</Button>
         </div>
       </div>
     );
   }
 }
 
-export default Exhibitionpage;
+const mapStateToProps = ({ authentication, merchant }: IRootState) => ({
+  account: authentication.account,
+  isAuthenticated: authentication.isAuthenticated,
+  merchantEntity: merchant.entity
+});
+
+const mapDispatchToProps = { getSession, getMyEntityMerchant, resetMerchant };
+
+type StateProps = ReturnType<typeof mapStateToProps>;
+type DispatchProps = typeof mapDispatchToProps;
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Exhibitionpage);
