84e2e4b30e2f18aec9f7a64f1fdf09122f71cdc5
Administrator <admin@example.com>
2019-07-29 18:47:00 +0800

    调整样式

--- src/main/webapp/app/modules/nearby/nearby.tsx
+++ src/main/webapp/app/modules/nearby/nearby.tsx
@@ -4,7 +4,7 @@ import { IRootState } from 'app/shared/reducers';
 import { getSession } from 'app/shared/reducers/authentication';
 
 import LongMenu from '../public/longmenu';
-import Nearbytabs from './nearbytabs';
+import Nearbylistbox from './nearbylistbox';
 
 /* vipkwd */
 import '../../../static/css/common.scss';
@@ -20,7 +20,7 @@ export class Nearby extends React.Component<INearbyProp> {
     return (
       <div ws-container-id="nearby">
         <LongMenu />
-        <Nearbytabs />
+        <Nearbylistbox />
       </div>
     );
   }
