119d9bd092353a2920054bfa1aad7a009f39fe5f
wxy545812093 <wxy545812093@126.com>
2019-07-30 21:12:58 +0800

    增加用户答疑问题中心-2

---
+++ src/main/webapp/app/modules/consumer/homelabelcard.tsx
@@ -0,0 +1,152 @@
+import React from 'react';
+// tslint:disable-next-line: no-submodule-imports
+import GridList from '@material-ui/core/GridList';
+// tslint:disable-next-line: no-submodule-imports
+import GridListTile from '@material-ui/core/GridListTile';
+// tslint:disable-next-line: no-submodule-imports
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+// tslint:disable-next-line: no-submodule-imports
+import IconButton from '@material-ui/core/IconButton';
+import { Link } from 'react-router-dom';
+import { IRootState } from 'app/shared/reducers';
+import { connect } from 'react-redux';
+import { getProducts } from 'app/requests/basic/result.reducer';
+import { getMyImgs } from 'app/requests/basic/files.reducer';
+
+class TitlebarGridList extends React.Component {
+  state = { products: [], files: [] };
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+      files: []
+    };
+  }
+  componentDidMount() {
+    // @ts-ignore
+    const product = this.props.getProducts(0, 10);
+    product.then(res => {
+      this.setState({
+        products: res.value.data.data
+      });
+      const arr = [];
+      res.value.data.data.map(elment => elment.specificationsDTO.map(spe => arr.push(spe.fileid)));
+      // @ts-ignore
+      const files = this.props.getMyImgs(arr);
+      // tslint:disable-next-line: no-shadowed-variable
+      files.then((res: { value: { data: any } }) => {
+        this.setState({
+          files: res.value.data
+        });
+      });
+    });
+    this.forceUpdate();
+  }
+  render() {
+    return (
+      <div>
+        <GridList cellHeight={180} style={{ margin: -0, width: '97%' }}>
+          {// @ts-ignore
+          this.state.products ? (
+            // @ts-ignore
+            this.state.products.map(tile =>
+              tile.specificationsDTO.map((specifications, index) => (
+                <Link
+                  to={{
+                    pathname: '/Productdetail',
+                    productid: specifications.id,
+                    postage: tile.postage,
+                    price: specifications.price,
+                    name: tile.name,
+                    json: specifications.specifications,
+                    model: specifications.model,
+                    num: specifications.num,
+                    // @ts-ignore
+                    img:
+                      this.state.files.length !== 0
+                        ? `data:${this.state.files[index].fileContentType};base64,${this.state.files[index].file}`
+                        : '',
+                    integral: specifications.integral
+                  }}
+                >
+                  <GridListTile
+                    key={specifications.titleimage}
+                    style={{ height: '200px', width: '50%', maxWidth: '100%', maxHeight: '100%', padding: 4 }}
+                  >
+                    {// @ts-ignore
+                    this.state.files.length !== 0 ? (
+                      <img
+                        style={{ height: '200px', width: '200px' }}
+                        // @ts-ignore
+                        src={`data:${this.state.files[index].fileContentType};base64,${this.state.files[index].file}`}
+                        alt={tile.name}
+                      />
+                    ) : (
+                      <img src="" />
+                    )}
+                    <GridListTileBar
+                      title={
+                        <p
+                          style={{
+                            wordBreak: 'break-all',
+                            whiteSpace: 'pre-wrap',
+                            wordWrap: 'break-word',
+                            overflow: 'hidden',
+                            display: '-webkit-box',
+                            lineHeight: '19px',
+                            height: '2.4rem',
+                            fontSize: '0.8rem',
+                            position: 'relative',
+                            margin: '0px'
+                          }}
+                        >
+                          {tile.name} + {specifications.specifications}
+                        </p>
+                      }
+                      subtitle={
+                        <span style={{ float: 'left', width: '100%' }}>
+                          <span style={{ float: 'left' }}>
+                            <span style={{ fontSize: '0.65rem' }}>￥:</span>
+                            {specifications.price}
+                          </span>
+                          <IconButton
+                            aria-label={`info about ${tile.name}`}
+                            style={{ height: '100%', outline: 'none', padding: 0, float: 'right', bottom: '5px' }}
+                          >
+                            …
+                          </IconButton>
+                        </span>
+                      }
+                    />
+                  </GridListTile>
+                </Link>
+              ))
+            )
+          ) : (
+            <p>网络繁忙</p>
+          )}
+        </GridList>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ authentication, files, result }: IRootState) => ({
+  filesEntity: files.entity,
+  filesEntitys: files.entities,
+  account: authentication.account,
+  resultEntity: result.entity
+});
+
+const mapDispatchToProps = {
+  getProducts,
+  getMyImgs
+};
+
+type StateProps = ReturnType<typeof mapStateToProps>;
+type DispatchProps = typeof mapDispatchToProps;
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TitlebarGridList);
---
+++ src/main/webapp/app/shared/menu/bottomnavigation.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { BottomNavigation, BottomNavigationAction, createStyles, makeStyles, Theme, Badge } from '@material-ui/core';
+import { StoreRounded, NearMeRounded, TextsmsRounded, AccountCircleRounded } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      margin: '0 auto',
+      width: '100%',
+      position: 'fixed',
+      bottom: '0px',
+      borderTop: '1px solid #f0f0f0',
+      height: '47px',
+      lineHeight: '18px',
+      '& button': {
+        minWidth: '50px',
+        paddingBottom: '0px',
+        paddingTop: '0px',
+        height: '46px',
+        '& span': {
+          minFontSize: '0.1rem',
+          fontSize: '0.65rem',
+          '& svg': {
+            paddingTop: '2px'
+          }
+        }
+      },
+      '& button.Mui-selected': {
+        color: '#fe4365',
+        fontSize: '0.65rem',
+        paddingTop: '0px',
+        outline: 'none',
+        '& span.Mui-selected': {
+          minFontSize: '0.1rem',
+          fontSize: '0.65rem',
+          paddingTop: '0px'
+        }
+      }
+    }
+  })
+);
+
+export default function LabelBottomNavigation() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState('consumer');
+
+  function handleChange(event: React.ChangeEvent<{}>, newValue: string) {
+    setValue(newValue);
+    document.getElementById('app-shared-menu-bottom-navigation-link-' + newValue).click();
+  }
+
+  return (
+    <BottomNavigation showLabels value={value} onChange={handleChange} className={classes.root}>
+      <BottomNavigationAction
+        label="首页"
+        value="consumer"
+        icon={
+          <Badge color="default" variant="dot">
+            <StoreRounded />
+          </Badge>
+        }
+      />
+      <Link id="app-shared-menu-bottom-navigation-link-consumer" to="/" />
+      <BottomNavigationAction
+        label="附近"
+        value="nearby"
+        icon={
+          <Badge color="default" variant="dot">
+            <NearMeRounded />
+          </Badge>
+        }
+      />
+      <Link id="app-shared-menu-bottom-navigation-link-nearby" to="/nearby" />
+      <BottomNavigationAction
+        label="消息"
+        value="information"
+        icon={
+          <Badge color="default" variant="dot">
+            <TextsmsRounded />
+          </Badge>
+        }
+      />
+      <Link id="app-shared-menu-bottom-navigation-link-information" to="/information" />
+      <BottomNavigationAction
+        label="我的"
+        value="personal"
+        icon={
+          <Badge color="default" variant="dot">
+            <AccountCircleRounded />
+          </Badge>
+        }
+      />
+      <Link id="app-shared-menu-bottom-navigation-link-personal" to="/personal" />
+    </BottomNavigation>
+  );
+}
